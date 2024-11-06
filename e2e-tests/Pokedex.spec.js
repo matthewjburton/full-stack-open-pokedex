860024bd3760a585a16bd7b6db920f37a2ae421e
matthewjburton <mattjburton64@gmail.com>
2024-11-06 10:53:20 -0500

    11.9

---
+++ e2e-tests/Pokedex.spec.js
@@ -0,0 +1,21 @@
+const { test, describe, expect } = require('@playwright/test')
+
+describe('Pokedex', () => {
+  test('front page can be opened', async ({ page }) => {
+    await page.goto('')
+    await expect(page.getByText('ivysaur')).toBeVisible()
+    await expect(
+      page.getByText(
+        'Pokémon and Pokémon character names are trademarks of Nintendo.'
+      )
+    ).toBeVisible()
+  })
+
+  test('can navigate to ivysaur page and see chlorophyll ability', async ({
+    page,
+  }) => {
+    await page.goto('/')
+    await page.click('text=ivysaur')
+    await expect(page.getByText('chlorophyll')).toBeVisible()
+  })
+})
